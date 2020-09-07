ba1441891c4fcf33c21eb5b754501cb1d3a14b1e
bycristhian <cristhian.2002.0@gmail.com>
2020-09-07 16:57:41 -0500

    Se integra ARcore y Dexter

--- app/src/main/java/com/sena/appculinariavirtual/ui/book/BookFragment.kt
+++ app/src/main/java/com/sena/appculinariavirtual/ui/book/BookFragment.kt
@@ -1,9 +1,11 @@
 package com.sena.appculinariavirtual.ui.book
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -13,18 +15,26 @@ import kotlinx.android.synthetic.main.fragment_book.view.*
 class BookFragment: Fragment() {
 
     private lateinit var bookViewModel: BookViewModel
+    private lateinit var fragmentContext: Context
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         bookViewModel = ViewModelProviders.of(this).get(BookViewModel::class.java)
 
         val root = inflater.inflate(R.layout.fragment_book, container, false)
+        fragmentContext = root.context
 
         bookViewModel.text.observe(viewLifecycleOwner, Observer {
-            root.content.text = it
+
         })
 
+        root.btnScanQr.setOnClickListener { goToScanQr() }
+
         return root
+    }
+
+    private fun goToScanQr(){
+        Toast.makeText(fragmentContext, "Scannning a Qr", Toast.LENGTH_LONG).show()
     }
 
 }
